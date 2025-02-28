f531224fd97ecee6df8983179363ba5715dacb74
leonette-dapaah <leonette.dapaah@ashesi.edu.gh>
2025-02-28 16:37:30 +0000

    inclusion of privacy policy

--- src/app/form-section/form-section.module.ts
+++ src/app/form-section/form-section.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { BasicInfoComponent } from './basic-info/basic-info.component';
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from "../shared/shared.module";
+import { PreviewComponent } from './preview/preview.component';
+import { BasicInfoComponent } from './basic-info/basic-info.component';
 import { BusinessInfoComponent } from './business-info/business-info.component';
 import { FormDocumentsComponent } from './form-documents/form-documents.component';
-import { PreviewComponent } from './preview/preview.component';
 
 @NgModule({
   imports: [
+    FormsModule,
+    CommonModule,
     SharedModule,
 ],
   declarations: [BasicInfoComponent, BusinessInfoComponent, FormDocumentsComponent, PreviewComponent],
--- src/app/form-section/preview/preview.component.ts
+++ src/app/form-section/preview/preview.component.ts
@@ -1,3 +1,4 @@
+import { Router } from "@angular/router";
 import { FormGroup } from "@angular/forms";
 import { Component, Input, OnInit } from "@angular/core";
 import { WizardService } from "src/app/core/wizard.service";
@@ -15,8 +16,9 @@ export class PreviewComponent implements OnInit {
   documents: any;
   businessInfo: any;
   avatarImage: string = "";
+  isChecked: boolean = false;
 
-  constructor(public wizardService: WizardService) { }
+  constructor(public wizardService: WizardService, private router: Router) { }
 
   averageMonthlyTransMap: { [key: string]: string } = {
     growing: "Under GHS 5,000",
@@ -33,5 +35,10 @@ export class PreviewComponent implements OnInit {
       const uploadedFile = this.businessInfo.logo;
       this.avatarImage = URL.createObjectURL(uploadedFile);
     }
+  }
+
+  navigateToPrivacyPolicy(event: Event) {
+    event.preventDefault();
+    this.router.navigate(["/privacy-policy"]);
   }
 }
