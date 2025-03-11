3d1d491b0b803ea947575e6f7a80eb5eba00d3dc
Cyprian Ampong Boateng <caboateng@itconsortiumgh.com>
2025-03-11 11:52:49 +0000

    Uncommented Sidebar code

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  appName: string = '';
+  constructor() {
+    this.appName = environment.APP_NAME;
+  }
 
   ngOnInit() {
   }
